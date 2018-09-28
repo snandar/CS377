2d79cd48a85bbf5839e5e382f34807943cba2fec
Nandar Soe <38772142+snandar@users.noreply.github.com>
2018-09-28 09:04:58 -0400

    Create inserttable.sql

---
+++ ProjectOne/inserttable.sql
@@ -0,0 +1,36 @@
+/*Insertion into Athlete Table*/
+
+INSERT INTO Athlete VALUES
+	('111111','AAA','Austria','11'),
+	('222222','BBB','Britain','22'),
+	('333333','CCC','Canada','33'),
+	('444444','DDD','Denmark','22'),
+	('555555','EEE','England','55');
+
+/*Insertion into Event Table*/
+
+INSERT INTO Event VALUES
+	('121212', 'Archery', '2018-1-12', 'Australia', '12:12:12'),
+	('131313', 'Basketball', '2018-1-13', 'Bangladesh', '13:13:13'),
+	('141414', 'Climbing', '2018-1-14', 'Cambodia', '14:14:14'),
+	('151515', 'Diving', '2018-1-15', 'Dominica', '15:15:15'),
+	('161616', 'Equestrian', '2018-1-16', 'Egypt', '16:16:16');
+
+/*Insertion into Participate Table*/
+
+INSERT INTO Participate VALUES
+	('111111','121212'),
+	('222222','131313'),
+	('333333','141414'),
+	('444444','151515'),
+	('555555','161616');
+
+/*Insertion into Medals Table*/
+
+INSERT INTO Medals VALUES
+	('111111', '121212', 'First'),
+	('222222','131313', 'Second'),
+	('333333','141414', 'Third'),
+	('111111','151515', 'First'),
+	('555555','161616', 'First');
+
